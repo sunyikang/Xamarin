25877761ba032b6739f528e02611e464b41291e5
Shuran <JIAN0066@gmail.com>
2015-04-09 21:11:16 +0800

    Adding of DAL layer (azure database)

---
+++ Tasky-2/TaskyWeb/DataObjects/Message.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskyWeb.DataObjects
+{
+    class Message : EntityData
+    {
+        public string Content { get; set; }
+        public bool IsSent { get; set; }
+
+        [ForeignKey("SenderId")]
+        public virtual User Sender { get; set; }
+
+        [ForeignKey("ReceiverId")]
+        public virtual User Receiver { get; set; }
+    }
+}
---
+++ Tasky-2/TaskyWeb/DataObjects/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace TaskyWeb.DataObjects
+{
+    class User : EntityData
+    {
+        public string Name { get; set; }
+    }
+}
