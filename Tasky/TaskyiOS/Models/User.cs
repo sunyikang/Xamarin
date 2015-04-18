f92f3021f8de7571227a699ee9cec56bb39b4c10
Shuran <jian0066@gmail.com>
2015-04-18 10:42:31 +0800

    Adding in the ID parameter azure

--- Tasky/TaskyiOS/Models/User.cs
+++ Tasky/TaskyiOS/Models/User.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using Tasky.BL.Contracts;
 
 namespace Tasky
 {
-	public class User
+	public class User 
 	{
+		//public int ID { get; set;}
 		public string Name { get; set;}
 		public string Password { get; set;}
+
+		public int id { get; set;}
 
 		public User (string name, string password)
 		{
