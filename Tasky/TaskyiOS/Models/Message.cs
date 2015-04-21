f21ead761972eec573f2b6819242557525df9ffc
Shuran <jian0066@gmail.com>
2015-04-21 18:47:17 +0800

    Add in the message data model

---
+++ Tasky/TaskyiOS/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Tasky
+{
+	public class Message
+	{
+		public string Id { get; set;}
+
+		public Message ()
+		{
+		}
+
+		public virtual User Sender { get; set; }
+	}
+}
+
