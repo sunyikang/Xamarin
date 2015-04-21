acfc2f09c3be9ba671094b8b8bb780348e03dd21
Shuran <jian0066@gmail.com>
2015-04-22 01:12:38 +0800

    Debug the server error with message model

--- Tasky/TaskyiOS/Models/Message.cs
+++ Tasky/TaskyiOS/Models/Message.cs
@@ -5,19 +5,30 @@ namespace Tasky
 {
 	public class Message
 	{
+		
 		public string Id { get; set; }
+
+		[JsonProperty(PropertyName = "SenderID")]
 		public string SenderID { get; set; }
+
+		[JsonProperty(PropertyName = "ReceiverID")]
 		public string ReceiverID { get; set; }
+
+		[JsonProperty(PropertyName = "Content")]
 		public string Content { get; set; }
+
+		[JsonProperty(PropertyName = "IsRead")]
 		public bool IsRead { get; set; }
+
+		[JsonProperty(PropertyName = "Order")]
 		public int Order { get; set; }
 
-		public Message (string SenderID, string ReceiverID, string Content, int Order)
+		public Message (string senderID, string receiverID, string content, int order)
 		{
-			this.SenderID = SenderID;
-			this.ReceiverID = ReceiverID;
-			this.Content = Content;
-			this.Order = Order;
+			this.SenderID = senderID;
+			this.ReceiverID = receiverID;
+			this.Content = content;
+			this.Order = order;
 		}
 	}
 }
