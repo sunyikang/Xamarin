e3547289d373a4d61613ebd49deea5543c775674
Shuran <jian0066@gmail.com>
2015-04-21 22:16:54 +0800

    Add Message Model on Client Side

---
+++ Tasky/TaskyiOS/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Tasky
+{
+	public class Message
+	{
+		public string Id { get; set; }
+		public string SenderID { get; set; }
+		public string ReceiverID { get; set; }
+		public string Content { get; set; }
+		public bool IsRead { get; set; }
+		public int Order { get; set; }
+
+		public Message (string SenderID, string ReceiverID, string Content, int Order)
+		{
+			this.SenderID = SenderID;
+			this.ReceiverID = ReceiverID;
+			this.Content = Content;
+			this.Order = Order;
+		}
+	}
+}
+
